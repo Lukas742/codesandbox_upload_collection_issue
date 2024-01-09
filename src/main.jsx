d17830a95deaa736a21cc5017286541b75c602a9
Lukas Harbarth <lukas.harbarth@sap.com>
2024-01-09 07:51:52 +0000

    Update 2 files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+import "@ui5/webcomponents-react/dist/Assets";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./main.css";
+
 const root = createRoot(document.getElementById("app"));
-import "@ui5/webcomponents-react/dist/Assets";
 
 root.render(<App />);
